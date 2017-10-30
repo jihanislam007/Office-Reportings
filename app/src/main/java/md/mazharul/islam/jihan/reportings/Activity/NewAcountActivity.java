da463223bcfab76b17d6419601bb657633f18afa
jihanislam007 <jihanislam007@gmail.com>
2017-10-31 01:51:37 +0600

    Password EditText Focus updated

--- app/src/main/java/md/mazharul/islam/jihan/reportings/Activity/NewAcountActivity.java
+++ app/src/main/java/md/mazharul/islam/jihan/reportings/Activity/NewAcountActivity.java
@@ -2,14 +2,153 @@ package md.mazharul.islam.jihan.reportings.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
 
 import md.mazharul.islam.jihan.reportings.R;
 
 public class NewAcountActivity extends AppCompatActivity {
+    EditText PasswordCarecterOneEditText,PasswordCarecterTwoEditText,PasswordCarecterThreeEditText,PasswordNumberOneEditText,PasswordNumberTwoEditText,PasswordNumberThreeEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_acount);
+
+        PasswordCarecterOneEditText= (EditText) findViewById(R.id.PasswordCarecterOneEditText);
+        PasswordCarecterTwoEditText= (EditText) findViewById(R.id.PasswordCarecterTwoEditText);
+        PasswordCarecterThreeEditText= (EditText) findViewById(R.id.PasswordCarecterThreeEditText);
+        PasswordNumberOneEditText= (EditText) findViewById(R.id.PasswordNumberOneEditText);
+        PasswordNumberTwoEditText= (EditText) findViewById(R.id.PasswordNumberTwoEditText);
+        PasswordNumberThreeEditText= (EditText) findViewById(R.id.PasswordNumberThreeEditText);
+
+        editTextFocusController();
     }
+
+    private void editTextFocusController() {
+        PasswordCarecterOneEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(s.length()>=1){
+                    PasswordCarecterOneEditText.clearFocus();
+                    PasswordCarecterTwoEditText.requestFocus();
+                    PasswordCarecterTwoEditText.setCursorVisible(true);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        PasswordCarecterTwoEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(s.length()>=1){
+                    PasswordCarecterTwoEditText.clearFocus();
+                    PasswordCarecterThreeEditText.requestFocus();
+                    PasswordCarecterThreeEditText.setCursorVisible(true);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        PasswordCarecterThreeEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(s.length()>=1){
+                    PasswordCarecterThreeEditText.clearFocus();
+                    PasswordNumberOneEditText.requestFocus();
+                    PasswordNumberOneEditText.setCursorVisible(true);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        PasswordNumberOneEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(s.length()>=1){
+                    PasswordNumberOneEditText.clearFocus();
+                    PasswordNumberTwoEditText.requestFocus();
+                    PasswordNumberTwoEditText.setCursorVisible(true);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        PasswordNumberTwoEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(s.length()>=1){
+                    PasswordNumberTwoEditText.clearFocus();
+                    PasswordNumberThreeEditText.requestFocus();
+                    PasswordNumberThreeEditText.setCursorVisible(true);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        PasswordCarecterOneEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(s.length()>=1){
+                    PasswordNumberThreeEditText.clearFocus();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+
+        // ,,,,
+    }
+
 }
