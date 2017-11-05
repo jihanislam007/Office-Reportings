8f32c25db5f3a4f683c3f21055115bb4d1d3e9fb
jihanislam007 <jihanislam007@gmail.com>
2017-11-05 07:56:06 +0600

    Setting page updated .

--- app/src/main/java/md/mazharul/islam/jihan/reportings/Activity/SettingsActivity.java
+++ app/src/main/java/md/mazharul/islam/jihan/reportings/Activity/SettingsActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
 
 import md.mazharul.islam.jihan.reportings.R;
@@ -11,20 +12,24 @@ import md.mazharul.islam.jihan.reportings.R;
 public class SettingsActivity extends AppCompatActivity {
 
 EditText SenderEmailAddress,
-    SenderEmailPassword;
+    SenderEmailPassword,
+    ReciverEmailAddress;
 ListView SettingListView;
 Button AddReciverMail;
+ImageView LessSecureImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-        SenderEmailAddress = (EditText) findViewById(R.id.EmailAddressEditText);
+        SenderEmailAddress = (EditText) findViewById(R.id.SenderEmailAddressEditText);
         SenderEmailPassword = (EditText) findViewById(R.id.EmailPasswordEditText);
+        ReciverEmailAddress = (EditText) findViewById(R.id.ReciverEmailAddressEditText);
 
         SettingListView = (ListView) findViewById(R.id.SettingListView);
         AddReciverMail = (Button) findViewById(R.id.EmailAddButton);
+        LessSecureImageView = (ImageView) findViewById(R.id.LessSecureImageView);
 
 
     }
